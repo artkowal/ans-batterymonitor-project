01c1152054c60acda8a96fdf4610094bc55fc7b4
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2024-04-27 19:31:13 +0200

    Easy bottom menu added.

--- app/src/main/java/com/example/ans_batterymonitor_project/MainActivity.java
+++ app/src/main/java/com/example/ans_batterymonitor_project/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.ans_batterymonitor_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.ans_batterymonitor_project.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.measurement) {
+                replaceFragment(new MeasurementFragment());
+            } else if (id == R.id.history) {
+                replaceFragment(new HistoryFragment());
+            } else if (id == R.id.setting) {
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
